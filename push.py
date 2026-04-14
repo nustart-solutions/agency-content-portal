230eab0706c26d6b4690a4f85cb2d5198c44a91b
Anne Allen <anne@nustart.solutions>
2026-04-14 07:38:12 -0700

    clean up push script

--- push.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-try:
-    subprocess.run(["git", "add", "."], check=True, text=True)
-    subprocess.run(["git", "commit", "-m", "update notifications component"], check=False, text=True)
-    res = subprocess.run(["git", "push"], check=True, capture_output=True, text=True)
-    print("SUCCESS: \n" + res.stdout + "\n" + res.stderr)
-except Exception as e:
-    print("FAILED: " + str(e))
